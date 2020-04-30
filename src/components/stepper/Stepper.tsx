7c59638988bc5661f0e0cf6a18add7cdf4b005d4
Mohit Tanwani <mohitmurlidhar@gmail.com>
2020-04-30 23:25:19 +0530

    :rocket: V1.0

--- src/components/stepper/Stepper.tsx
+++ src/components/stepper/Stepper.tsx
@@ -5,7 +5,7 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Radio, RadioGroup, FormControlLabel, TextField } from '@material-ui/core';
+import { Radio, RadioGroup, FormControlLabel, TextField, FormControl, InputLabel, OutlinedInput } from '@material-ui/core';
 import { MailContext, IEmail } from '../../context/MailContext';
 
 
@@ -21,6 +21,9 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(1),
 
     },
+    margin: {
+      margin: theme.spacing(1),
+    },
   }),
 );
 
@@ -31,10 +34,17 @@ function getSteps() {
 
 export default function HorizontalLabelPositionBelowStepper() {
   const [value, setValue] = React.useState('Regarding');
+  const [salutation, setSalutation] = React.useState('Hi');
   const [textBox, setTextBox] = React.useState("");
+  const [textBox2, setTextBox2] = React.useState("");
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
+  const [body, setBody] = React.useState("");
+  const [signOf, setSignOf] = React.useState("Thankyou");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
 
   const Mail: IEmail = React.useContext(MailContext)!;
 
@@ -43,8 +53,13 @@ export default function HorizontalLabelPositionBelowStepper() {
     setValue((event.target as HTMLInputElement).value);
   };
 
+  const handleSalutationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextBox2("");
+    setSalutation((event.target as HTMLInputElement).value);
+  };
+
   const handleNext = () => {
-    if (activeStep === 0)
+    if (activeStep === 0) {
       if (value !== "10")
         Mail.setMail({
           ...Mail.mail,
@@ -55,7 +70,51 @@ export default function HorizontalLabelPositionBelowStepper() {
           ...Mail.mail,
           subject: textBox
         });
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      if (textBox.length > 0)
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      else
+        alert("Fill the Subject");
+    }
+    else if (activeStep === 1) {
+      if (salutation !== "10")
+        Mail.setMail({
+          ...Mail.mail,
+          salutation: salutation + " " + textBox2 + ","
+        });
+      else
+        Mail.setMail({
+          ...Mail.mail,
+          salutation: textBox2 + ","
+        });
+      if (textBox2.length > 0 || salutation === "Greetings")
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      else
+        alert("Fill the Salutation Properly");
+    }
+    else if (activeStep === 2) {
+      if (body.length > 0) {
+        Mail.setMail({
+          ...Mail.mail,
+          body: body
+        });
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      }
+      else
+        alert("Fill the body properly");
+    }
+    else if (activeStep === 3) {
+      if (name.length > 0) {
+        Mail.setMail({
+          ...Mail.mail,
+          ending: signOf + "\n" + name
+        });
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setEmail("Subject : " + Mail.mail.subject + "\n\n" + Mail.mail.salutation + "\n\n" + Mail.mail.body + "\n\n" + signOf + "\n" + name);
+      }
+      else
+        alert("Don't Forget to write your Name !");
+    }
+
   };
 
   const handleBack = () => {
@@ -65,6 +124,18 @@ export default function HorizontalLabelPositionBelowStepper() {
   const handleReset = () => {
     setActiveStep(0);
   };
+  const copy = () => {
+    var textField = document.createElement('textarea')
+    textField.innerText = email
+    document.body.appendChild(textField)
+    textField.select()
+    document.execCommand('copy')
+    textField.remove()
+    // navigator.clipboard.writeText(email);
+    alert('Mail Copied !!');
+  }
+
+
 
   return (
     <div className={classes.root}>
@@ -77,11 +148,24 @@ export default function HorizontalLabelPositionBelowStepper() {
       </Stepper>
       <div>
         {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>All steps completed</Typography>
-            <Button onClick={handleReset}>Reset</Button>
-            <Button onClick={handleReset} variant="contained" color="primary">Send Email</Button>
+          <>
+          <div className="left">
+            <FormControl fullWidth className={classes.margin} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-amount">Email</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-amount"
+                value={email}
+                multiline
+                rows={20}
+                labelWidth={60}
+              />
+            </FormControl>
           </div>
+          <div className="right">
+          <Button onClick={handleReset}>Reset</Button>
+          <Button className={classes.margin} variant="contained" onClick={copy} color="primary">Copy Email</Button>
+          </div>
+          </>
         ) : (
             <div>
 
@@ -153,6 +237,149 @@ export default function HorizontalLabelPositionBelowStepper() {
                         disabled={value !== "10" ? true : false} />}
                     />
                   </RadioGroup>
+                </div>
+
+              }
+              {
+                activeStep === 1 &&
+                <div className="left">
+                  <RadioGroup aria-label="body" name="body" value={salutation} onChange={handleSalutationChange}>
+
+                    <FormControlLabel value="Hi" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Hi </Typography>}
+                      label={<TextField id='r1' placeholder='John'
+                        variant='standard' autoComplete="off"
+                        onChange={(e) => setTextBox2(e.target.value)} disabled={salutation !== "Hi" ? true : false}
+                        value={salutation === "Hi" ? textBox2 : ""} />} />}
+                    />
+
+                    <FormControlLabel value="Dear" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Dear </Typography>}
+                      label={<TextField id='r1' placeholder='Maria'
+                        variant='standard' autoComplete="off"
+                        onChange={(e) => setTextBox2(e.target.value)} value={salutation === "Dear" ? textBox2 : ""}
+                        disabled={salutation !== "Dear" ? true : false} />} />}
+                    />
+
+                    <FormControlLabel value="Respected" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Respected </Typography>}
+                      label={<TextField id='r1' placeholder='Authoroties'
+                        variant='standard' autoComplete="off"
+                        onChange={(e) => setTextBox2(e.target.value)} value={salutation === "Respected" ? textBox2 : ""}
+                        disabled={salutation !== "Respected" ? true : false} />} />}
+                    />
+
+                    <FormControlLabel value="Hello" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Hello </Typography>}
+                      label={<TextField id='r1' placeholder='John'
+                        variant='standard' autoComplete="off"
+                        onChange={(e) => setTextBox2(e.target.value)} disabled={salutation !== "Hello" ? true : false}
+                        value={salutation === "Hello" ? textBox2 : ""} />} />}
+                    />
+
+                    <FormControlLabel value="Greetings" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Greetings, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="10" control={<Radio />}
+                      label={<TextField id="r4" onChange={(e) => setTextBox2(e.target.value)} placeholder="Write your own "
+                        autoComplete="off" variant="standard" value={value === "10  " ? textBox2 : ""}
+                        disabled={salutation !== "10" ? true : false} />}
+                    />
+                  </RadioGroup>
+                </div>
+              }
+              {
+                activeStep === 2 &&
+                <div className="left">
+                  <FormControl fullWidth className={classes.margin} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-amount">Body</InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-amount"
+                      value={body}
+                      onChange={(e) => setBody(e.target.value)}
+                      multiline
+                      rows={20}
+                      labelWidth={60}
+                    />
+                  </FormControl>
+                </div>
+              }
+              {
+                activeStep === 3 &&
+                <div className="left">
+                  <RadioGroup aria-label="body" name="body" value={signOf} onChange={(e) => setSignOf(e.target.value)}>
+                    <FormControlLabel value="Thankyou" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Thankyou, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Yours Faithfully" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Yours Faithfully, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Yours Truely" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Yours Truely, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Best" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Best, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Best Wishes" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Best Wishes, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Respectfully" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Respectfully, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Regards" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Regards, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Take Care" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Take Care, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Thankyou in Advance" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Thankyou in Advance, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Cheers" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Cheers, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Good Luck" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Good Luck, </Typography>}
+                      label="" />}
+                    />
+
+                    <FormControlLabel value="Have a great Day" control={<Radio />} label={<FormControlLabel
+                      control={<Typography className={classes.instructions}> Have a great Day, </Typography>}
+                      label="" />}
+                    />
+                    <TextField
+                      id="outlined-multiline-static"
+                      label="Your Name"
+                      multiline
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      rows={2}
+                      variant="outlined"
+                    />
+                  </RadioGroup>
+
                 </div>
               }
               <div className="right">
